1db111de7cd2c8ceb07269663f48987f9736d338
lmsnsk <lmslms87@yandex.ru>
2025-03-14 15:55:40 +0700

    r43r3

--- src/components/Header/Links.jsx
+++ src/components/Header/Links.jsx
@@ -3,7 +3,7 @@ import boltImg from "./../../assets/images/bolts.svg";
 import beamImg from "./../../assets/images/beam.svg";
 import eyeletImg from "./../../assets/images/eyelet.svg";
 import springImg from "./../../assets/images/spring-icon.svg";
-// import tolerancesImg from "./../../assets/images/tolerances.svg";
+import tolerancesImg from "./../../assets/images/tolerances.svg";
 
 const Links = ({ effect }) => {
   return (
@@ -12,7 +12,7 @@ const Links = ({ effect }) => {
       <NavItem link="/Beams" text="Расчет балок" effect={effect} img={beamImg} />
       <NavItem link="/BoltGroup" text="Расчёт группы болтов" effect={effect} img={boltImg} />
       <NavItem link="/Spring" text="Расчёт пружин" effect={effect} img={springImg} />
-      <NavItem link="/Tolerances" text="Допуски и посадки" effect={effect} img={springImg} />
+      <NavItem link="/Tolerances" text="Допуски и посадки" effect={effect} img={tolerancesImg} />
     </>
   );
 };
